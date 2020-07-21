8b38276b61eff47dbc9e0e738285ceaa837d0bf4
William Han Findiesen <william.findiesen@gmail.com>
2020-07-21 03:53:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,8 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+ BRANDS = ["Uggs","Rainbow","Nike"]
 
   def initialize(brand)
     @brand = brand
@@ -10,5 +12,12 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  def brand=(brand)
+    @brand = brand
+    
+    Shoe::BRANDS.include?(brand) ? nil : BRANDS << brand 
+  end
+    
 
 end
